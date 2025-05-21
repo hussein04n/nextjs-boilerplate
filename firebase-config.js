15ea85daff9b656b1872f7d8b63e1b34bbef1264
hussein04n <hussein04n@outlook.com>
2025-05-21 10:13:13 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBfaMxkaF-QRPdf24gYCJKeLMarNNQLaBQ",
+  authDomain: "storage-5ba27.firebaseapp.com",
+  projectId: "storage-5ba27",
+  storageBucket: "storage-5ba27.firebasestorage.app",
+  messagingSenderId: "911744755254",
+  appId: "1:911744755254:web:d35cd9ee5ed4e36638da97",
+  measurementId: "G-WFPBJDZ1RB"
+};
+
+firebase.initializeApp(firebaseConfig);
